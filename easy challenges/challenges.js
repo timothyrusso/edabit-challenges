25a5cfa684c5b22b169b01953415b5430fa4b34b
timothyrusso <russotimothy@live.it>
2022-06-14 07:19:20 +0200

    feat: new challenge

--- easy challenges/challenges.js
+++ easy challenges/challenges.js
@@ -10,3 +10,31 @@ function addUp(num) {
 
 /* ------------------------------------------------------------------------------------ */
 
+/*
+Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+*/
+
+function minMax(arr) {
+  let minimum = Math.min(...arr);
+  let maximum = Math.max(...arr);
+  let result = [minimum, maximum];
+  return result;
+}
+
+// or
+
+function minMax(arr) {
+  let maximum = arr[0];
+  let minimum = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (maximum < arr[i]) {
+      maximum = arr[i];
+    } else {
+      minimum = arr[i];
+    }
+  }
+  let result = [minimum, maximum];
+  return result;
+}
+
+/* ------------------------------------------------------------------------------------ */
